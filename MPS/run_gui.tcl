d8c1da95b2e7e810ff651256e42b1ed6733fd22a
Romain POUILLARD <119610372+rppouill@users.noreply.github.com>
2023-05-19 14:48:00 +0200

    Update run_gui.tcl

--- MPS/run_gui.tcl
+++ MPS/run_gui.tcl
@@ -1,20 +1,21 @@
 #!/usr/bin/tclsh
-# Use me as : vsim -do run_gui.tcl
+# Use me as : vsim -do run_cli.tcl
+# TODO
 proc compile_design {} {
   puts " \t >> Compiling project files ..."
-  vlib work
-  vmap work work
-  vcom -2008 -work work test.vhd
-  vcom -2008 -work work testbench.vhd
+  vlib -unix <work placeholder>/work
+  vmap <work placeholder>/work
+  vcom -2008 -work <work placeholder>/work /home/ropouillard/Documents/Thesis_Working/Simulateur/Frist/MPS/test.vhd
+  vcom -2008 -work <work placeholder>/work /home/ropouillard/Documents/Thesis_Working/Simulateur/Frist/MPS/testbench.vhd
 }
 
 proc sim_design {} {
   puts " \t >> Performing RTL Sim ..."
-  vsim -f args.txt work.testbench
-  #set msp_lib /home/kamel/dev/etc/modelsim_utils
-  #source $msp_lib/auto_wave.tcl
-  source auto_wave.tcl
-  run 2 ms
+  #vsim work.testbench
+  vsim -f <args placeholder> <work placeholder>/work.testbench
+  vcd file results.vcd
+  vcd add -r *
+  run 5 ms
 }
 
 proc exit_sim  {} {
@@ -24,6 +25,7 @@ proc exit_sim  {} {
 proc all {} {
   compile_design
   sim_design
+
 }
 
 all
